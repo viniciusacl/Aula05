61b339f446acc7175a47ea5515d9c8cf62db0c35
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-15 15:41:23 -0300

    Eviando Alterações no Exercico05

--- Exercicio05.py
+++ Exercicio05.py
@@ -3,6 +3,6 @@ Valor1 = int(input("Digite o Primeiro Valor: "))
 Valor2 = int(input("Digite o Segundo Valor: "))
 Divisao=0
 while Valor2 == 0:
-    print("Valor Incorreto!!!")
+    Valor2 = int(print("Digite o Segundo Valor:"))
 Divisao = Valor1 / Valor2
 print(Divisao)  
