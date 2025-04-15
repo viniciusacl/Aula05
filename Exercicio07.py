583451ce203b7ccfdbb7375b8d6704e05c0ecb2d
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-15 16:55:29 -0300

    Alteração no Cod, para calcular novamente com while.

--- Exercicio07.py
+++ Exercicio07.py
@@ -1,12 +1,16 @@
 #Escrever um Cod para ler as notas da 1a e 2a. Av de um Aluno, calcular e imprimir a média. Aceitar valores de (0 a 10).
-Nota1 = float(input("Digite a Nota da 1a. Av: "))
-while Nota1 <0 or Nota1>10:
+Novamente="S"
+while Novamente == "s":
     Nota1 = float(input("Digite a Nota da 1a. Av: "))
-Nota2 = float(input("Digite a Nota da 2a. Av: "))
-while Nota2 <0 or Nota1>10:
+    while Nota1 <0 or Nota1>10:
+        Nota1 = float(input("Digite a Nota da 1a. Av: "))
     Nota2 = float(input("Digite a Nota da 2a. Av: "))
-Media=(Nota1+Nota2/2)
-print(Media)
+    while Nota2 <0 or Nota1>10:
+        Nota2 = float(input("Digite a Nota da 2a. Av: "))
+    Media=(Nota1+Nota2/2)
+    print(Media)
+    Novamente = input("Deseja Cálcular Novamente?(s/n) ")
+
 
 
 
